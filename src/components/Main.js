1eab5be0cf0deb6dd9f29b9cbd36ed7ac638557c
Zakaria Ouarrhi <zakaria555@hotmail.com>
2022-07-07 09:46:10 +0200

    day-workout

---
+++ src/components/Main.js
@@ -0,0 +1,114 @@
+import {nanoid} from 'nanoid';
+import {useState, useEffect} from 'react';
+
+import data from '../data.json';
+import StyledMain from '../style/StyledMain';
+
+export default function Form() {
+  const [selectedBodypart, setSelectedBodypart] = useState('');
+  const [plans, setPlans] = useState([]);
+  const [weeks, setWeeks] = useState(JSON.parse(localStorage.getItem('my_plan')) || []);
+
+  //handlesubmit
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    saveWeek();
+  }
+  //setting the day workout
+  function createPlan(event) {
+    const newPlan = event.target.value;
+    setPlans([...plans, {name: newPlan, id: nanoid()}]);
+  }
+
+  // workouts store
+
+  useEffect(() => {
+    localStorage.setItem('my_plan', JSON.stringify(weeks));
+  }, [weeks]);
+
+  //setting the sets
+  function updateSets(index, value) {
+    setPlans([...plans.slice(0, index), {...plans[index], sets: value}, ...plans.slice(index + 1)]);
+  }
+
+  //save multiple workouts
+  function saveWeek() {
+    const newSet = {exercise: plans, bodyPart: selectedBodypart};
+    setWeeks([...weeks, newSet]);
+    setPlans([]);
+    setSelectedBodypart('');
+  }
+
+  //delete day-plan
+  function deletMyPlan(index) {
+    setWeeks([...weeks.slice(0, index), ...weeks.slice(index + 1)]);
+  }
+
+  //filtering data from the json
+  const allBodyParts = Object.values(data);
+  const filteredExercises = data.find(workout => workout.parts === selectedBodypart)?.exercises;
+  //const allExercises = exercisesGroups.flat(Infinity);
+
+  return (
+    <StyledMain>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="input1">Body part</label>
+        <select name="input1" value={selectedBodypart} onChange={event => setSelectedBodypart(event.target.value)}>
+          <option key={nanoid()}>Choose a body part:</option>
+          {allBodyParts.map(part => (
+            <option key={part.id}>{part.parts}</option>
+          ))}
+        </select>
+        {filteredExercises && (
+          <>
+            <label htmlFor="input2">Exercise:</label>
+            <select name="input2" onChange={createPlan}>
+              <option value={'default'}>Choose an exercise</option>
+              {filteredExercises?.map(exercise => (
+                <option key={exercise.id}>{exercise.name}</option>
+              ))}
+            </select>
+          </>
+        )}
+        <ul>
+          {plans.map((plan, index) => (
+            <li key={plan.id} className="workout-plan">
+              {plan.name}
+              <input
+                name={'plan' + plan.id}
+                className="sets-input"
+                min="1"
+                type="number"
+                onChange={event => updateSets(index, event.target.value)}
+              />
+            </li>
+          ))}
+        </ul>
+        <button type="submit">button</button>
+      </form>
+      <div className="weeks-plan">
+        {weeks.map((week, index) => (
+          <div className="day-plan" key={nanoid()}>
+            <h3>{week.bodyPart}</h3>
+            <ul key={week.id}>
+              {week.exercise.map(workout => (
+                <li key={workout.id} className="workout-plan2">
+                  {workout.name}: {workout.sets}
+                </li>
+              ))}
+              <button
+                className="done-button"
+                onClick={() => {
+                  deletMyPlan(index);
+                }}
+              >
+                Done
+              </button>
+            </ul>
+          </div>
+        ))}
+      </div>
+    </StyledMain>
+  );
+}
---
+++ src/style/StyledMain.js
@@ -0,0 +1,70 @@
+import styled from 'styled-components';
+
+const StyledMain = styled.main`
+  form {
+    display: grid;
+    text-align: center;
+    gap: 1rem;
+  }
+  h2 {
+    display: flex;
+    justify-content: center;
+  }
+  select {
+    width: 11rem;
+    height: 1.5rem;
+    margin: auto;
+  }
+  .workout-plan {
+    list-style: none;
+    display: flex;
+    justify-content: space-between;
+  }
+  .workout-plan2 {
+    list-style: none;
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+  }
+  ul {
+    min-width: 200px;
+
+    width: fit-content;
+  }
+  .setsInput {
+    width: 2rem;
+    height: 0.8rem;
+    margin-left: 1.5rem;
+  }
+  button {
+    margin: 2rem auto;
+    font-size: 1.1rem;
+    width: 6rem;
+    height: 2rem;
+    background-color: #6e64;
+    border-radius: 7px;
+    border: none;
+  }
+  .done-button {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .weeks-plan {
+    border: 1px solid;
+    display: flex;
+    overflow-x: auto;
+  }
+  .day-plan {
+    display: flex;
+    flex-direction: column;
+
+    margin-right: 0.5rem;
+    border-radius: 7px;
+  }
+  h3 {
+    text-align: center;
+    text-decoration: underline;
+  }
+`;
+export default StyledMain;
